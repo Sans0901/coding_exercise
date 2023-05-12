273dec49263a32d411d55d2b60758c835f643278
Sanskruti Sandbhor <43126620+Sans0901@users.noreply.github.com>
2023-05-12 20:15:55 +0530

    11052023

--- List/4 Climbing stairs.py
+++ List/4 Climbing stairs.py
@@ -12,3 +12,21 @@ class Solution:
             second = first
             first = temp
         return first
+
+'''
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+'''
+
+
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        fib = [0, 1]
+        for i in range(2, n+1):
+            fib.append(fib[i-1] + fib[i-2])
+        return fib[n]
+                        

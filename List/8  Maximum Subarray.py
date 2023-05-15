fdff94c082e9dd38392883a6f10f31d19f4bf2f9
Sanskruti Sandbhor <43126620+Sans0901@users.noreply.github.com>
2023-05-15 22:17:00 +0530

    15052023

---
+++ List/8  Maximum Subarray.py
@@ -0,0 +1,25 @@
+'''
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+'''
+
+
+
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float('-inf')  
+        current_sum = 0 
+        
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+        
+        return max_sum

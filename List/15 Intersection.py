d7b485a75a1f86181b69cf35c4f4fb352f997f4f
Sanskruti Sandbhor <43126620+Sans0901@users.noreply.github.com>
2023-05-24 10:33:52 +0530

    23052023

---
+++ List/15 Intersection.py
@@ -0,0 +1,63 @@
+'''
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+'''
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        output = []
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] == nums2[j]:
+                    if nums1[i] not in output:
+                        output.append(nums1[i])
+        return output
+        
+  
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:  
+        set1 = set(nums1)
+        set2 = set(nums2)
+        intersection = set1.intersection(set2)
+        return list(intersection)
+        
+'''
+Given two integer arrays nums1 and nums2, return an array of their intersection.
+Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+'''
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dict = {}
+        output = []
+        for ele in nums1:
+            if ele in dict:
+                dict[ele] = dict[ele] + 1
+            else:
+                dict[ele] = 1
+
+        for ele in nums2:
+            if ele in dict and dict[ele] > 0:
+                output.append(ele)
+                dict[ele] = dict[ele] - 1
+
+        return output

d00a5d4f7ab898c3b039c1db0819f630886da6a3
Sanskruti Sandbhor <43126620+Sans0901@users.noreply.github.com>
2023-07-31 22:49:05 +0530

    310723

--- List/32 Largest no twice of other.py
+++ List/32 Largest no twice of other.py
@@ -27,3 +27,53 @@ class Solution:
                 dominant_idx = i
                 
         return dominant_idx
+        
+'''
+Keep Multiplying Found Values by Two
+
+Example 1:
+
+Input: nums = [5,3,6,1,12], original = 3
+Output: 24
+Explanation: 
+- 3 is found in nums. 3 is multiplied by 2 to obtain 6.
+- 6 is found in nums. 6 is multiplied by 2 to obtain 12.
+- 12 is found in nums. 12 is multiplied by 2 to obtain 24.
+- 24 is not found in nums. Thus, 24 is returned.
+Example 2:
+
+Input: nums = [2,7,9], original = 4
+Output: 4
+Explanation:
+- 4 is not found in nums. Thus, 4 is returned.
+'''
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original *= 2
+        return original
+        
+'''
+1346. Check If N and Its Double Exist
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+'''
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+        for num in arr:
+            if num * 2 in seen or num / 2 in seen:
+                return True
+            seen.add(num)
+        return False

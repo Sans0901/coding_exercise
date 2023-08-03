cf187c03112bb5ef5a07c35b0aa6e33d84164048
Sanskruti Sandbhor <43126620+Sans0901@users.noreply.github.com>
2023-08-03 06:08:20 +0530

    020823

--- List/32 Largest no twice of other.py
+++ List/32 Largest no twice of other.py
@@ -77,3 +77,29 @@ class Solution:
                 return True
             seen.add(num)
         return False
+
+'''
+977. Squares of a Sorted Array
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+'''      
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        op = [0] * len(nums)
+        for i in range(len(nums)):
+            op[i] = nums[i] * nums[i]  
+        op.sort()
+        return op
+       
+# Optimized solution
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range (len(nums)):
+            nums[i]=nums[i]*nums[i]
+        nums.sort()
+        return nums
+    
